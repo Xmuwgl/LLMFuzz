16b4936f28f959d293956d447200431b245cfe8f
Xmuwgl <87184825+Xmuwgl@users.noreply.github.com>
2025-05-12 14:19:29 +0800

    Create daemon.py

---
+++ daemon.py
@@ -0,0 +1,8 @@
+def realign():
+    '''Rollback when new generator failed to run, rollback to `generator_backup`'''
+    pass
+
+      
+def collect_feedback():
+    '''Collect multifuzz's feedback like coverage information'''
+    pass
